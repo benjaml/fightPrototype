16b40bfdf1eaf247fabeb724cd487703f2bcad12
judcitron <ju.dcitron@hotmail.fr>
2016-12-01 18:19:42 +0100

    res
    ajout resistance si ennemi meurt

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -36,6 +36,67 @@ public class Enemy : MonoBehaviour {
 	
 	}
 
+    void takeDamages (AttackEnum attaque, int damages)
+    {
+        switch (attaque)
+        {
+            case AttackEnum.PHYSICAL_FIRE:
+                if (physic_fire == true)
+                {
+                    return;
+                }
+                break;
+
+            case AttackEnum.PHYSICAL_THUNDER:
+                if (physic_thunder == true)
+                {
+                    return;
+                }
+                break;
+
+            case AttackEnum.PHYSICAL_ICE:
+                if (physic_ice == true)
+                {
+                    return;
+                }
+                break;
+
+            case AttackEnum.MAGICAL_FIRE:
+                if (magic_fire == true)
+                {
+                    return;
+                }
+                break;
+
+            case AttackEnum.MAGICAL_THUNDER:
+                if (magic_thunder == true)
+                {
+                    return;
+                }
+                break;
+
+            case AttackEnum.MAGICAL_ICE:
+                if (magic_ice == true)
+                {
+                    return;
+                }
+                break;
+        }
+
+        hp = hp - damages;
+
+        if (hp <= 0)
+        {
+            Death(attaque);
+        }
+    }
+
+    void Death(AttackEnum ae)
+    {       
+        EnemyManager.instance.ApplyRes(ae);
+    }
+
+
     public float getScore()
     {
         return 0;
