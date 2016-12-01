1dceb6a2e9ca35b21d707ce0536450822c5a3bbf
ETU\b.lefevre <benjaml>
2016-12-01 18:19:40 +0100

    playerObserver

---
+++ Assets/PlayerObserver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerObserver : MonoBehaviour {
+
+    List<AttackEnum> playerAttacks = new List<AttackEnum>();
+
+    public static PlayerObserver instance = null;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void RegisterAttack(AttackEnum ae)
+    {
+        if (!playerAttacks.Contains(ae))
+            playerAttacks.Add(ae);
+    }
+
+}
